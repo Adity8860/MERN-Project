96771fd548392c9e59cac95fa0222fa5a8325f4a
Aditya Gaur <gauradi355@gmail.com>
2024-10-29 22:54:48 +0530

    commit

--- models/listings.model.js
+++ models/listings.model.js
@@ -15,7 +15,10 @@ const listingSchema = new Schema({
   // },
   image: {
     filename: String,
-    url: String
+    url: {
+      type: String,
+      default:"https://images.unsplash.com/photo-1579285466670-006e66e6055e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
   },
   // image: {
   //   type: String,
