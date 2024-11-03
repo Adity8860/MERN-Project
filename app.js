3f58c5c970fc01c987cb5c3ec84daef462c5e30d
Aditya Gaur <gauradi355@gmail.com>
2024-11-03 17:41:50 +0530

    commit

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listings.model.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMates = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 const MONGODB_URI = "mongodb://localhost:27017/wanderlust2";
 
@@ -29,67 +31,86 @@ app.get("/", (req, res) => {
 });
 
 //Index Route
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
 //New Route
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
 //Show Route
-app.get("/listings/:id", async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+  })
+);
 
 //Create Route
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    if (!req.body.listing) {
+      throw new ExpressError("Invalid Listing Data", 400);
+    }
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-});
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
-  const { id } = req.params;
-  await Listing.findByIdAndUpdate(id, {
-    ...req.body.listing
-  });
-  res.redirect(`/listings/${id}`);
-});
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+      throw new ExpressError("Invalid Listing Data", 400);
+    }
+    const { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {
+      ...req.body.listing,
+    });
+    res.redirect(`/listings/${id}`);
+  })
+);
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
-  const { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
 });
 
-/**/ /* */
-// app.get("/testListings", async (req, res) => {
-//   let sampleListings = new Listing({
-//     title: "Sample Listing",
-//     description: "This is a sample listing",
-//     image:
-//       "https://images.unsplash.com/photo-1729180801744-c26a031be578?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//     price: 100,
-//     location: "Sample Location",
-//     country: "Sample Country",
-//   });
-
-//   await sampleListings.save();
-//   console.log("successfully saved sample listing");
-//   res.send("test successful");
-// });
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  if (statusCode !== 404) {
+    console.error(err); // Log the error to the console
+  }
+  res.status(statusCode).render("error.ejs", { message });
+});
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
